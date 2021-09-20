b56a1463ee20444f635e182448d3f5f3081194c3
Diogo Chaves <diogo@enfoquevisual.com.br>
2021-09-20 08:58:52 -0300

    description

--- get_next_line.c
+++ get_next_line.c
@@ -6,6 +6,37 @@ char	*get_next_line(int fd)
 	static int	pos;
 	int	i;
 
+
+/*
++ static *buffer;
+
+- [BUFFER?]
+--- Iterate BUFFER, search for a new line '\n'
+--- [newline?]
+----- MALLOC var LINE(sizeof BUFFER until "\n"): copy content from BUFFER to
+      LINE
+----- Copy content from BUFFER (position (newline + 1) to sizeof(BUFFER)) to a
+      new var (TEMP)
+----- copy temp to new buffer
+----- return LINE
+--- [no newline?]
+----- MALLOC var FILE(BUFFER_SIZE + len(BUFFER) (+ 1 [for the '\0'] ??))
+----- Copy from BUFFER to FILE.
+- Read from FD into FILE (pass *(FILE + len(BUFFER)) as pointer location)
+- Iterate FILE, search for a new line '\n'
+- [newline]
+--- MALLOC var LINE(sizeof FILE until "\n"): copy content from FILE to LINE
+--- Copy content from FILE (position (newline + 1) to sizeof(FILE)) to BUFFER
+--- return LINE
+- [no newline?]
+--- copy content from FILE to BUFFER ( BUFFER += file)
+--- start again
+- [EOF?]
+--- MALLOC var LINE(sizeof FILE until "EOF"): copy content from FILE to LINE
+--- return LINE
+*/
+
+
 	pos = 0;
 	// first call: allocate buffer
 	if (!file)
